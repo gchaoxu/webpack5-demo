2c3f4374e1b5d40167530a5c498c64b966733127
Alec <xgc0829@163.com>
2022-08-23 22:09:39 +0800

    comment: webpack 配置文件

--- 02_webpack_config_start/webpack.config.js
+++ 02_webpack_config_start/webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.js', //可以是相对路径
   output: {
     filename: 'build.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist')  //输出位置要求是绝对路径
   }
 }
