2c6d2dd868be3d2e49b4b92f41d5c33f4b246568
Alec <xgc0829@163.com>
2022-08-28 09:27:47 +0800

    webpack-dev-middleware

--- 23_webapck_middle_ware/Server.js
+++ 23_webapck_middle_ware/Server.js
@@ -6,8 +6,9 @@ const app = express()
 
 // 获取配置文件
 const config = require('./webpack.config.js')
+// 配置文件传递给 webpack 去打包并返回打包之后的结果
 const compiler = webpack(config)
-
+// 打包之后的结果传递给中间件
 app.use(webpackDevMiddleware(compiler))
 
 // 开启端口上的服务监听
